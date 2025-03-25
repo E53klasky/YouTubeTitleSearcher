571c05a0bb5d415bb0323fa5b1bd67ecba129e27
Ethan Klasky <klaskyethan@gmail.com>
2025-03-25 18:36:39 -0400

    made it better should be good now

--- my-app/src/dataScrutures/hashmap.js
+++ my-app/src/dataScrutures/hashmap.js
@@ -1,81 +1,4 @@
-class ytVideoStatsHashmap {
-    // i will change the size later 
-    constructor(bucketSize = 262144) { //2^18
-        this.buckets = new Array(bucketSize);
-        this.bucketSize = bucketSize; 
-    }
-
-    hash(key) {
-        let hashValue = 0;
-        for (let i = 0; i < key.length; i++) {
-            hashValue += key.charCodeAt(i) * i;
-        }
-        return hashValue % this.buckets.length; 
-    }
-
-    setItem(key, value) {
-        const index = this.hash(key);
-        if (!this.buckets[index]) {
-            this.buckets[index] = [];
-        }
-        
-        for (let i = 0; i < this.buckets[index].length; i++) {
-            if (this.buckets[index][i][0] === key) {
-                this.buckets[index][i][1] = value;
-                return;
-            }
-        }
-        
-        this.buckets[index].push([key, value]);
-    }
-
-    getItem(key) {
-        const index = this.hash(key);
-        if (!this.buckets[index]) {
-            return null;
-            // throw new Error('Key not found'); not sure
-        }
-
-        for (let pair of this.buckets[index]) {
-            if (pair[0] === key) {
-                return pair[1];
-            }
-        }
-        
-        return null;
-        // throw new Error('Key not found'); not sure
-    }
-
-    removeItem(key) {
-        const index = this.hash(key);
-        if (!this.buckets[index]) {
-            return false; 
-        // throw new Error('Key not found'); not sure
-        }
-
-        for (let i = 0; i < this.buckets[index].length; i++) {
-            if (this.buckets[index][i][0] === key) {
-                this.buckets[index].splice(i, 1);
-                return true;
-            }
-        }
-        
-        return false; 
-        // throw new Error('Key not found'); not sure
-    }
-    
-    forEach(callback) {
-        for (let i = 0; i < this.buckets.length; i++) {
-            if (this.buckets[i]) {
-                for (let pair of this.buckets[i]) {
-                    callback(pair[1], pair[0]);
-                }
-            }
-        }
-    }
-}
-
-
+// VideoStats class to store video information
 class VideoStats {
     constructor(title, likes, comments, views) {
         this.title = title;
@@ -84,26 +7,108 @@ class VideoStats {
         this.views = views;
     }
 }
-// testing it waiting to for real test 
-const ytVideoStats = new ytVideoStatsHashmap();
 
-ytVideoStats.setItem('video1', new VideoStats('Cool Video 1', 100, 20, 1000));
-ytVideoStats.setItem('video2', new VideoStats('Awesome Video 2', 200, 40, 2000));
-ytVideoStats.setItem('video3', new VideoStats('Amazing Video 3', 300, 60, 3000));
+class OptimizedYTVideoStatsHashmap {
+    constructor(bucketSize = 131072) { // Optimal bucket size for load factor control 100,000
+        this.buckets = new Array(bucketSize);
+        this.bucketSize = bucketSize;
+        this.size = 0;
+    }
 
+    // Hash function to map video IDs to bucket indices
+    hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = ((hash << 5) - hash) + key.charCodeAt(i);
+            hash |= 0; 
+        }
+        return Math.abs(hash * 16777619) % this.bucketSize;
+    }
 
-console.log("Video 1 stats:", ytVideoStats.getItem('video1'));
+    // Insert or update a video entry in the hashmap
+    setItem(videoId, videoStats) {
+        if (!(videoStats instanceof VideoStats)) {
+            throw new Error('Value must be an instance of VideoStats');
+        }
 
+        // Resize if load factor exceeds 0.7
+        if (this.size / this.bucketSize > 0.7) {
+            this._resize(this.bucketSize * 2);
+        }
 
-ytVideoStats.setItem('video1', new VideoStats('Updated Video 1', 150, 25, 1500));
-console.log("Updated video 1 stats:", ytVideoStats.getItem('video1'));
+        const index = this.hash(videoId);
+        if (!this.buckets[index]) {
+            this.buckets[index] = [];
+        }
 
+        // Check if video ID already exists and update it
+        for (let i = 0; i < this.buckets[index].length; i++) {
+            if (this.buckets[index][i][0] === videoId) {
+                this.buckets[index][i][1] = videoStats;
+                return;
+            }
+        }
 
-console.log("Removing video2:", ytVideoStats.removeItem('video2'));
-console.log("Trying to get removed video:", ytVideoStats.getItem('video2'));
+        // Add new video entry
+        this.buckets[index].push([videoId, videoStats]);
+        this.size++;
+    }
 
+    // Resize the hashmap when necessary
+    _resize(newSize) {
+        const oldBuckets = this.buckets;
+        this.buckets = new Array(newSize);
+        this.bucketSize = newSize;
+        this.size = 0;
 
-console.log("All videos:");
-ytVideoStats.forEach((value, key) => {
-    console.log(`${key}: ${value.title} - ${value.likes} likes, ${value.comments} comments, ${value.views} views`);
-});
+        for (let bucket of oldBuckets) {
+            if (bucket) {
+                for (let [videoId, videoStats] of bucket) {
+                    this.setItem(videoId, videoStats);
+                }
+            }
+        }
+    }
+
+    // Retrieve video stats by video ID
+    getItem(videoId) {
+        const index = this.hash(videoId);
+        if (!this.buckets[index]) {
+            return null;
+        }
+
+        for (let [storedVideoId, videoStats] of this.buckets[index]) {
+            if (storedVideoId === videoId) {
+                return videoStats;
+            }
+        }
+        
+        return null;
+    }
+
+    // Remove a video entry by video ID
+    removeItem(videoId) {
+        const index = this.hash(videoId);
+        if (!this.buckets[index]) {
+            return false;
+        }
+
+        for (let i = 0; i < this.buckets[index].length; i++) {
+            if (this.buckets[index][i][0] === videoId) {
+                this.buckets[index].splice(i, 1);
+                this.size--;
+    
+                // Shrink hashmap 
+                if (this.size > 0 && this.size / this.bucketSize < 0.2) {
+                    this._resize(Math.max(this.bucketSize / 2, 32768)); 
+                }
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+module.exports = { OptimizedYTVideoStatsHashmap, VideoStats };

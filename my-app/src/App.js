6c0aac20ca7acffc506dc72ec54cabe6f3a3833f
OwenS3881 <owenski08@gmail.com>
2025-03-25 23:45:00 -0400

    Implemented Trie data structure and wrote some test cases for it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import LandingPage from './components/landingPage';
+import React from "react";
+import "./App.css";
+import LandingPage from "./components/landingPage";
+import TrieTesting from "./TrieTesting";
 
 function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <LandingPage />
+            <TrieTesting />
+        </div>
+    );
 }
 
 export default App;

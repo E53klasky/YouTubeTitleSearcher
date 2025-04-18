8884c404f083964e89090790bfab070b96543411
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-04-18 09:46:17 -0400

    can search mutliple times

--- my-app/src/components/buildTrie.js
+++ my-app/src/components/buildTrie.js
@@ -1,34 +1,31 @@
-
-
 export function buildTrie(words) {
-    const root = {
-      char: "",
-      isEnd: false,
-      children: {},
-    };
-  
-    for (const word of words) {
-      let current = root;
-  
-      for (let i = 0; i < word.length; i++) {
-        const char = word[i];
-  
-        if (!current.children[char]) {
-          current.children[char] = {
-            char: char,
-            isEnd: false,
-            children: {},
-          };
-        }
-  
-        current = current.children[char];
-  
-        // Mark end of word
-        if (i === word.length - 1) {
-          current.isEnd = true;
-        }
+  const root = {
+    char: '',
+    isEnd: false,
+    children: Object.create(null), 
+  };
+
+  for (const word of words) {
+    let current = root;
+
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+
+      if (!current.children[char]) {
+        current.children[char] = {
+          char,
+          isEnd: false,
+          children: Object.create(null),
+        };
+      }
+
+      current = current.children[char];
+
+      if (i === word.length - 1) {
+        current.isEnd = true;
       }
     }
-  
-    return root;
   }
+
+  return root;
+}
--- my-app/src/components/doAnalyze.js
+++ my-app/src/components/doAnalyze.js
@@ -60,26 +60,42 @@ export async function doAnalyze(
         setCurrentWord(word);
 
         const trieStart = performance.now();
-        const trieStats = trie.getWordData(words[0]);
+        const trieStats = trie.getWordData(word); 
         const trieEnd = performance.now();
 
         const mapStart = performance.now();
-        const hashmapStats = hashmap.getAverageStatsForWord(words[0]);
+        const hashmapStats = hashmap.getAverageStatsForWord(word); 
         const mapEnd = performance.now();
 
-        setDataDisplay(
-            `The word \"${word}\" has a score of ${hashmapStats}. It has an average views of ${trieStats.views}, an average likes of ${trieStats.likes}, and an average comments of ${trieStats.comments}`
-        );
-        setTrieTime(
-            `The Trie produced the results in ${
-                trieEnd - trieStart
-            }ms for the word \"${word}\"`
-        );
-        setMapTime(
-            `The Hash Map produced the results in ${
-                mapEnd - mapStart
-            }ms for the word \"${word}\"`
-        );
+        if (trieStats && hashmapStats) {
+            setDataDisplay(
+                `The word "${word}" has a score of ${hashmapStats}. It has an average views of ${trieStats.views}, an average likes of ${trieStats.likes}, and an average comments of ${trieStats.comments}`
+            );
+            setTrieTime(
+                `The Trie produced the results in ${
+                    trieEnd - trieStart
+                }ms for the word "${word}"`
+            );
+            setMapTime(
+                `The Hash Map produced the results in ${
+                    mapEnd - mapStart
+                }ms for the word "${word}"`
+            );
+        } else {
+            setDataDisplay(
+                `No data found for the word "${word}"`
+            );
+            setTrieTime(
+                `The Trie search took ${
+                    trieEnd - trieStart
+                }ms for the word "${word}" but found no results`
+            );
+            setMapTime(
+                `The Hash Map search took ${
+                    mapEnd - mapStart
+                }ms for the word "${word}" but found no results`
+            );
+        }
 
         try {
             await cancelableDelay(

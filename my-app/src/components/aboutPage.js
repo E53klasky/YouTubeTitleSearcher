6d03cbe7a7f86beb48b8885b8417dfd2fd3d2015
kingwhatthe <sabalschuster06@gmail.com>
2025-04-22 13:01:55 -0400

    Adjusted about page website discription

--- my-app/src/components/aboutPage.js
+++ my-app/src/components/aboutPage.js
@@ -85,8 +85,8 @@ function AboutPage() {
                             }}
                             color={whiteColor}
                         >
-                            YouTube Title Optimization Website Tools Used
-                            Material UI React.js Project Overview: We are
+                            YouTube Title Analzyer uses
+                            Material UI, React.js, and Framer Motion. Project Overview: We are
                             developing a frontend-only website that compares the
                             efficiency of two data structures: a Trie and a
                             HashMap. Our goal is to visualize and analyze their
@@ -101,11 +101,9 @@ function AboutPage() {
                             Title Optimization for YouTubers: We analyze YouTube
                             video titles based on metrics like comments, views,
                             and likes to help content creators optimize their
-                            titles for better engagement. Data Source: The
-                            dataset used for this project was sourced via
-                            YouTube's video ID dataset from 2019 (newest) and
-                            YouTube's API for finding the video title, comments,
-                            views, and likes.
+                            titles for better engagement. Data Source: We used over 100,000 random videos from
+                            YouTube's API and found the video title, comments,
+                            views, and likes for each video.
                         </Typography>
                     </Box>
                     <Box>
